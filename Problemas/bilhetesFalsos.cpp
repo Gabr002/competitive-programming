ef080a2fabf663aebad9b453d021fafb4ca4daed
Dantas_Dev02 <98811096+Gabr002@users.noreply.github.com>
2023-04-29 00:11:10 -0300

    Resolução de mais 2 problemas.
    - Bilhetes falsos.
    - Nove.

---
+++ Problemas/bilhetesFalsos.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    
+    long long int N, M, count = 0, repetido = 0;
+
+    while(cin >> N >> M){
+        int vetor[10000];
+        set<int> num;
+
+        for(int i = 0; i < M; i++){
+            cin >> vetor[i];
+            for(int j = 0; j < i; j++){
+                if(vetor[i] == vetor[j])
+                    num.insert(vetor[i]);
+            }
+        }
+
+        cout << num.size() << endl;
+
+        count = 0;
+    }
+
+}
---
+++ Problemas/nove.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int size, exp;
+
+    while(cin >> size){
+
+        for(int i = 0; i < size; i++){
+
+            cin >> exp;
+            (exp % 2 == 0) ? '1\n' : '9\n';
+
+        }
+    }
+}
