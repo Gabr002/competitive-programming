26b5e78591692f1164b8600e4f3ad2e6e3e4d2e1
Gabriel Dantas <98811096+Gabr002@users.noreply.github.com>
2025-05-02 13:26:37 -0300

    update: up questions done

--- Problemas Uri e Neps Academy/Homework-0-2024/abc285_brutmhyhiizp.cpp
+++ Problemas Uri e Neps Academy/Homework-0-2024/abc285_brutmhyhiizp.cpp
@@ -2,17 +2,6 @@
 using namespace std;
 typedef long long ll;
 
-int pot(ll e){  
-  if(e == 0) return 1;
-  ll p = 1;
-
-  for(int i = 1; i <= e; i++){
-    p*=26;
-  }
-
-  return p;
-}
-
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -20,10 +9,7 @@ int main(){
   ll sum = 0;
   cin >> s;
   
-  for(int i = 0, e = s.size()-1; i < s.size(); i++, e--){
-    ll id = s[i];
-    sum += ((id-64)*pot(e));
-  }
+  for(int i = 0, e = s.size()-1; i < s.size(); i++, e--) sum += s[i]*26;
   
   cout << sum << endl;
 
