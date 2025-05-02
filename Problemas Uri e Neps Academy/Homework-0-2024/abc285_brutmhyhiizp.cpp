64b9ae3e900749713142f423521d1df0d8c73f6d
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-05-02 01:21:18 -0300

    feat: up questions done

--- Problemas Uri e Neps Academy/Homework-0-2024/abc285_brutmhyhiizp.cpp
+++ Problemas Uri e Neps Academy/Homework-0-2024/abc285_brutmhyhiizp.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-// AA
-// 1 x 26¹ + 1 x 26⁰ = 27
-int pot(int e){
-    if(e == 0) return 1;
-    int p = 1;
-    for(int i = 1; i <= e; i++){
-        p *= 26;
-    }    
+int pot(ll e){  
+  if(e == 0) return 1;
+  ll p = 1;
 
-    return p;
+  for(int i = 1; i <= e; i++){
+    p*=26;
+  }
+
+  return p;
 }
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    string s;
-    int sum = 0;
-    cin >> s;
-    
-    for(int i = 0, e = s.size()-1; i < s.size(); i++, e--){
-        int id = s[i];
-        sum += (id-64)*pot(e);
-    }
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  ll sum = 0;
+  cin >> s;
+  
+  for(int i = 0, e = s.size()-1; i < s.size(); i++, e--){
+    ll id = s[i];
+    sum += ((id-64)*pot(e));
+  }
+  
+  cout << sum << endl;
 
-    cout << sum << endl;
-
-    return 0;
+  return 0;
 }
