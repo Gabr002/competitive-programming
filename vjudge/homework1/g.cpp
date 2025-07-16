097beeb406af78b36c42274db8af19f0ec4b0282
gabr002 <gdantaslopes62@gmail.com>
2025-07-16 05:35:46 -0300

    up data

--- vjudge/homework1/g.cpp
+++ vjudge/homework1/g.cpp
@@ -1,17 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define _ ios_base::sync_with_stdio(0); cin.tie(0);
 #define f first
 #define s second
-
+ 
 typedef long long ll;
-
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-
+ 
+// const int INF = 0x3f3f3f3f;
+// const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+ 
+ll bs(vector<ll>& vi, ll targ){
+    ll l = 0, r = vi.size()-1, pos = -1;
+    
+    while(l<=r){
+        ll mid = (l+r)/2;
+        if(vi[mid] == targ){
+            pos = mid;
+            r = mid - 1;
+        }
+        else if(vi[mid] < targ) l = mid+1;
+        else r = mid - 1;
+    }
+    
+    return pos;
+}
+ 
 int main(){ _
-    vector<int> v={2, 4, 7, 7, 9};
-    cout << v[5];
+    ll n, q; scanf("%lld%lld", &n, &q);
+    vector<ll> vi;
+    
+    for(int i = 0; i < n; i++){
+        ll ai; scanf("%lld", &ai);
+        vi.push_back(ai);
+    }  
+ 
+    while(q--){
+        ll s; scanf("%lld", &s);
+        printf("%lld\n", bs(vi, s));
+    }
     return 0;
 }
