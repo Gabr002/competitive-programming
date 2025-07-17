84e700e5ae2006cff5624af5d7def4a6ed9aae7b
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-07-17 06:19:50 -0300

    Minhas mudanças no h.cpp antes do pull

--- vjudge/homework1/h.cpp
+++ vjudge/homework1/h.cpp
@@ -11,6 +11,30 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 int main(){ _
-    
+    int t; scanf("%d", &t);
+
+    while(t--){
+        int n, xi, yi; scanf("%d", &n);
+        vector<pair<int, int>> ordv(n); 
+        vector<int> cnt(n);
+
+        for(int i = 0; i < n; i++){
+            scanf("%d%d", &xi, &yi);
+            cnt[xi]++;
+            ordv[i].second = yi;
+        }
+
+        int cur = 0;
+
+        for(int i = 0; i < cnt.size(); i++){
+            while(cnt[i]){
+                ordv[cur].first = i;
+                cur++;
+                cnt[i]--;
+            }
+        }
+
+        for(int i = 0; i < n; i++) printf("%d %d\n", ordv[i].first, ordv[i].second);
+    }
     return 0;
 }
