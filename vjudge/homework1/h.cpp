131fbe6704a1b1113d336fd9bf82aaf50aa897c2
gabr002 <gdantaslopes62@gmail.com>
2025-07-17 05:27:10 -0300

    up

--- vjudge/homework1/h.cpp
+++ vjudge/homework1/h.cpp
@@ -10,7 +10,29 @@ typedef long long ll;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
+vector<pair<int, int>> v;
+
+bool cmp(auto a, auto b){
+    return ((a > b) && (a.first > b.first) || (a.first == b.first && a.second < b.second));
+}
+
+
 int main(){ _
-    
+    int t; scanf("%d", &t);
+
+    while(t--){
+        int n, xi, yi; scanf("%d", &n);
+        
+        for(int i = 0; i < n; i++){
+            scanf("%d%d", &xi, &yi);
+            v.push_back({xi, yi});
+        }
+
+        sort(v.begin(), v.end(), cmp);
+
+        for(int i = 0; i < n; i++){
+            printf("%d %d\n", v[i].first, v[i].second);
+        }
+    }
     return 0;
 }
