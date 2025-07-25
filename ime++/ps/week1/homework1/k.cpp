c5fd3f5217b7f944aafaf4e86271b0d4f4ae2d59
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-07-25 20:14:07 -0300

    fix: problem no solved

--- ime++/ps/week1/homework1/k.cpp
+++ ime++/ps/week1/homework1/k.cpp
@@ -12,6 +12,7 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 int main(){ _
     int t; cin >> t;
+    bool check = false;
     string _template, str_guardada;
 
     while(t--){
@@ -19,24 +20,24 @@ int main(){ _
         
         if(!_template.empty()){
             str_guardada = _template;
-            for(int i = 0; i < _template.size(); i+=2){
-                for(int j = 0; j < str.size(); j+=2){
+            check = true;
+            for(int i = 0; i < _template.size(); i++){
+                for(int j = 0; j < str.size(); j++){
                     if(i < _template.size()-1 && j < str.size()-1){
-                        cout << i << j << "\n";
-                        if(str[j] == _template[i] && str[j+1] ==  _template[i+1]) continue;
-                        else{
-                            str_guardada.push_back(str[j]);
-                            str_guardada.push_back(str[j+1]);   
+                        //if(str[j] == _template[i] && str[j+1] ==  _template[i+1]){ 
+                        if(str[j] == _template[i]){
+                            // Construir substrings
                         }
                     }
                 }
             }
+            str_guardada += str;
         }else{
             _template = str;
         }
     }
 
-    cout << str_guardada.size() << "\n";
-    cout << str_guardada << "\n";
+    if(check) cout << str_guardada.size() << "\n";
+    else cout << _template.size() << "\n";
     return 0;
 }
