edc8c001341c39278f63cf5e45821d3f4b30c50b
gabr002 <gdantaslopes62@gmail.com>
2025-08-02 06:29:57 -0300

    fix: any update repo

--- ime++/ps/week1/homework1/k.cpp
+++ ime++/ps/week1/homework1/k.cpp
@@ -11,8 +11,8 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 
 int main(){ _
-    int t; cin >> t;
-    bool check = false;
+    int t, _begin = 0, _end = 0; cin >> t;
+    bool check = false, check_begin = true, check_end = false, check_erase = false;
     string _template, str_guardada;
 
     while(t--){
@@ -23,12 +23,26 @@ int main(){ _
             check = true;
             for(int i = 0; i < _template.size(); i++){
                 for(int j = 0; j < str.size(); j++){
-                    if(i < _template.size()-1 && j < str.size()-1){
-                        //if(str[j] == _template[i] && str[j+1] ==  _template[i+1]){ 
-                        if(str[j] == _template[i]){
-                            // Construir substrings
-                        }
+                    if(str[j] == _template[i] && str[j+1] ==  _template[i+1] && check_begin){
+                        _begin = j;
+                        check_begin = false; // Até que encontre o check_end.
+                        check_end = true; // Até que ache o ultimo j+1;
+                        break; // Para ele percorrer o proximo
+                    }else if(str[j] == _template[i] && str[j+1] ==  _template[i+1] && check_end && str[j+2] !=  _template[i+2]){
+                        _end = j+1;
+                        check_begin = true; // Nova procura.
+                        check_end = false; // Até que ache o proximo begin;
+                        check_erase = true;
+                        i = 0;
+                        break;
+                    }else if(i == _template.size()-1 && str[j] == _template[i] && str[j-1] ==  _template[i-1]){
+                        _end = j;
+                        check_erase = true;
                     }
+                }
+                if(check_erase){ 
+                    str.erase(_begin, _end-_begin+1);
+                    check_erase = false;
                 }
             }
             str_guardada += str;
