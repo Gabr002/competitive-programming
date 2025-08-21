b03924647c6749ba270e3e3747d5a00214f66e2d
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-08-21 19:10:13 -0300

    up problem solved

--- ufmg/notebook-problems/basicos/multiplicationdilemma.cpp
+++ ufmg/notebook-problems/basicos/multiplicationdilemma.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #define _ ios_base::sync_with_stdio(0); cin.tie(0);
 #define f first
-#define s second
+#define s second 
 
 typedef long long ll;
 #define endl "\n";
@@ -14,48 +14,51 @@ const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 int main(){ _
     int t; scanf("%d", &t);
     
-        vector<ll> v_a, v_b;
-
-    // Para o primeiro produto
-
     while(t--){
         ll a, b, c = 1, d = 1; scanf("%lld%lld", &a, &b);
+        vector<ll> v_a, v_b;
 
-        while(a > 0){
-            ll mod_a = a%10;    
+        if(a == 0 || b == 0){ 
+            printf("%lld\n", a*0);
+            continue;
+        }
+        
+        /*
+        input:
+        2
+        55 20
+        70 17
+        */
 
-            mod_a *= c;
+        /*
+        Output:
+        60 x 20 - 5 x 20
+        -3 x 70 + 70 X 20
+        */
+        
+        while(a != 0){ // 20
+            ll mod_a = a%10; // first case test 
+             
+            mod_a *= c; 
             a = a/10;
-            if(mod_a == 0){ 
-                 v_a.push_back(10);
-                if(a<=9){  
-                    v_a.push_back(a);
-                    break;
-                }        
-            }
-            else if(a > 0) v_a.push_back(mod_a);
-            else v_a.push_back(mod_a);
+            if(mod_a == 0){
+                c *= 10;
+                continue; 
+            }else if(a != 0) v_a.push_back(mod_a);
+            else v_a.push_back(mod_a); 
             c *= 10;
         }
-
-        // Para o segundo produto
-
-        while(b > 0){
-            // 20
-            ll mod_b = b%10; // 0
-
-            mod_b *= d; // 0
+     
+        while(b != 0){ 
+            ll mod_b = b%10; 
+     
+            mod_b *= d; 
             b = b/10;
-            if(mod_b == 0){ 
-                v_b.push_back(10); // 10
-                if(b<=9){ 
-                    v_b.push_back(b);
-                    break;
-                }
-            }
-            else if(b > 0) v_b.push_back(mod_b);
+            if(mod_b == 0){
+                d *= 10;
+                continue;
+            }else if(b != 0) v_b.push_back(mod_b);
             else v_b.push_back(mod_b);
-            
             d *= 10;
         }
 
@@ -68,6 +71,8 @@ int main(){ _
             }
         }
     }
+ 
     
+    // Para o segundo produto   
     return 0;
 }
