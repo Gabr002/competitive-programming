891be4a494b525ad31b4a71093cd508fc211c0fa
Gabriel Dantas <gdantaslopes62@gmail.com>
2025-09-08 17:53:44 -0300

    update all files

--- ufmg/notebook-problems/basicos/fatorial.cpp
+++ ufmg/notebook-problems/basicos/fatorial.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
 #define f first
 #define s second
 
@@ -9,15 +10,23 @@ typedef long long ll;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int unsigned MOD = 1e6;
 
-int fat(int n){
-    if(n == 0 || n == 1) return 1;
+ll fat(ll N){
+    long long x = 1;
 
-    return n*fat(n-1);
+    for (int i = 2; i <= N; i++){
+        x *= i;
+        while (x % 10 == 0) x /= 10;   
+        x %= MOD;
+    }
+
+    return x%10;
 }
 
 int main(){ _
-    int n; cin >> n;
-    cout << fat(n) << "\n";
+    int n; scanf("%d", &n);
+
+    printf("%lld\n", fat(n));
     return 0;
 }
